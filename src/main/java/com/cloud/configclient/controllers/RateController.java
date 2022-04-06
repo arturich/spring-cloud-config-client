1bdaf500a6e7fab1978b7c9225b4c89b224a1020
arturich <arturich@gmail.com>
2022-04-06 11:31:09 -0500

    Added configuration to receive encrypted configurations and decript

--- src/main/java/com/cloud/configclient/controllers/RateController.java
+++ src/main/java/com/cloud/configclient/controllers/RateController.java
@@ -1,11 +1,13 @@
 package com.cloud.configclient.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @Controller
 @RequestMapping("/client")
 public class RateController {
@@ -19,12 +21,16 @@ public class RateController {
     @Value("${tollstart}")
     String tollstart;
 
+    @Value("${connstring}")
+    String connstring;
+
     @GetMapping("/rate")
     public String getRate(Model theModel)
     {
         theModel.addAttribute("rateamount",rate);
         theModel.addAttribute("lanes",lanecount);
         theModel.addAttribute("tollstart",tollstart);
+        theModel.addAttribute("connstring",connstring);
 
         return "rateview";
     }
